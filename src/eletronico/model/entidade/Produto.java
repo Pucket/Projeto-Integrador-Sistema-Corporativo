07937ab2c2d53c901d354ac39d5be8718818e0ab
edson <edsonfreitas499@gmail.com>
2020-12-04 19:30:21 -0300

    Produto Entidade

--- src/eletronico/model/entidade/Produto.java
+++ src/eletronico/model/entidade/Produto.java
@@ -10,11 +10,65 @@ package eletronico.model.entidade;
  * @author edson
  */
 public class Produto {
+    
     private int codProduto;
     
     private String nome;
     
+    private double preco;
+    
+    private int quantidade;
+    
     private Categoria categoria;
     
     private Marca marca;
+
+    public int getCodProduto() {
+        return codProduto;
+    }
+
+    public void setCodProduto(int codProduto) {
+        this.codProduto = codProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+    
 }
